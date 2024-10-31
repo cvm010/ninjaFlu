08822308e42db172c78c8dcd9fd11110d06004db
CVM <s778193047@gmail.com>
2024-10-31 14:26:22 +0530

    basic site using widgets and row , column

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text(
+            'Ninja Id Card',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        )
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+         child : Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget>[
+            Center(
+            child : CircleAvatar(
+               backgroundImage: AssetImage('assets/pr1.jpeg'),
+               radius: 40,
+             ),
+            ),
+             Divider(
+               height: 60,
+               color : Colors.grey[800],
+             ),
+             Text(
+               'NAME',
+               style: TextStyle(
+                 color: Colors.grey,
+                 letterSpacing: 2,
+                 fontWeight: FontWeight.bold,
+               ),
+             ),
+             SizedBox(height: 10),
+             Text(
+               'Chun-li',
+               style: TextStyle(
+                 color: Colors.amberAccent[200],
+                 letterSpacing: 2,
+                 fontSize: 28,
+                 fontWeight: FontWeight.bold,
+               ),
+             ),
+             SizedBox(height: 30),
+
+             Text(
+               'CURRENT NINJA LEVEL',
+               style: TextStyle(
+                 color: Colors.grey,
+                 letterSpacing: 2,
+                 fontWeight: FontWeight.bold,
+               ),
+             ),
+             SizedBox(height: 10),
+             Text(
+               '8',
+               style: TextStyle(
+                 color: Colors.amberAccent[200],
+                 letterSpacing: 2,
+                 fontSize: 28,
+                 fontWeight: FontWeight.bold,
+               ),
+             ),
+
+             SizedBox(height: 30),
+             Row(
+               children: <Widget>[
+                 Icon(
+                 Icons.email,
+                 color: Colors.grey[400],
+                 ),
+               SizedBox(width: 10),
+                 Text(
+                   'sk7784@gmail.com',
+                   style: TextStyle(
+                     color: Colors.grey[400],
+                     fontSize: 18,
+                     letterSpacing: 1,
+                   ),
+                 )
+               ],
+             )
+
+           ],
+         ),
+      )
+    );
+  }
+}
